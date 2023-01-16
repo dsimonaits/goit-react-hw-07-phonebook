55f43582ea5e72da4b1eb25f33b02bb055ae6ae7
Deniss Simonaits <dsimonaits@gmail.com>
2023-01-16 22:46:34 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,30 @@
-export const App = () => {
+import { useDispatch } from 'react-redux';
+import { useEffect } from 'react';
+import Section from './Section';
+import ContactForm from './ContactForm';
+import FilterByName from './Filter';
+import ContactList from './ContactList';
+import { fetchContacts } from 'redux/operations';
+
+const App = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchContacts());
+  }, [dispatch]);
+
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <>
+      <Section title="Phonebook" border="false">
+        <ContactForm />
+      </Section>
+
+      <Section title="Contacts" border="true">
+        <FilterByName />
+        <ContactList />
+      </Section>
+    </>
   );
 };
+
+export default App;
