859debe571fc15c4e6853ebee7012a20accd1236
Deniss Simonaits <dsimonaits@gmail.com>
2023-01-17 00:12:22 +0200

    Update ContactList.jsx

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,23 +1,15 @@
 import { useSelector } from 'react-redux';
-import { getContacts, getFilter } from 'redux/selectors';
+import { selectFilteredContacts } from 'redux/selectors';
 import Contact from 'components/Contact';
 import { List } from './ContactList.styled';
 import { ListItem } from './ContactList.styled';
 
 const ContactList = () => {
-  const contacts = useSelector(getContacts);
-  const filter = useSelector(getFilter);
-
-  const filterContactsByName = () => {
-    const normalizedFilter = filter.toLowerCase();
-    return contacts.filter(contact =>
-      contact.name.toLocaleLowerCase().includes(normalizedFilter)
-    );
-  };
+  const filteredContacts = useSelector(selectFilteredContacts);
 
   return (
     <List>
-      {filterContactsByName().map(({ name, number, id }) => (
+      {filteredContacts.map(({ name, number, id }) => (
         <ListItem key={id}>
           {' '}
           <Contact name={name} number={number} id={id} />
