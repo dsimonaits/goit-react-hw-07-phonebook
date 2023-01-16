00855009f04ac6092cc52a5c31758a9da3db2cb1
Deniss Simonaits <dsimonaits@gmail.com>
2023-01-16 22:47:07 +0200

    Create operations.js

---
+++ src/redux/operations.js
@@ -0,0 +1,41 @@
+import axios from 'axios';
+import { createAsyncThunk } from '@reduxjs/toolkit';
+
+axios.defaults.baseURL = 'https://63c400fe8067b6bef6d1bace.mockapi.io';
+
+export const fetchContacts = createAsyncThunk(
+  'contacts/fetchContacts',
+  async (_, { rejectWithValue }) => {
+    try {
+      const { data } = await axios('/contacts');
+      return data;
+    } catch (error) {
+      return rejectWithValue(error);
+    }
+  }
+);
+
+export const deleteContact = createAsyncThunk(
+  'contacts/deleteContact',
+  async (id, { rejectWithValue }) => {
+    try {
+      await axios.delete(`/contacts/${id}`);
+      return id;
+    } catch (error) {
+      return rejectWithValue(error);
+    }
+  }
+);
+
+export const addContact = createAsyncThunk(
+  'contacts/addContact',
+  async (contact, { rejectWithValue }) => {
+    try {
+      const { data } = await axios.post('/contacts', contact);
+      console.log(data);
+      return data;
+    } catch (error) {
+      rejectWithValue(error);
+    }
+  }
+);
