f56933944c7225babf0da84bee292f0d62293383
Deniss Simonaits <dsimonaits@gmail.com>
2023-01-17 00:12:28 +0200

    Update selectors.js

--- src/redux/selectors.js
+++ src/redux/selectors.js
@@ -1,3 +1,18 @@
-export const getFilter = state => state.filter.filter;
-export const getContacts = state => state.contacts.items;
-export const getIsLoading = state => state.contacts.isLoading;
+import { createSelector } from '@reduxjs/toolkit';
+
+export const selectFilter = state => state.filter.filter;
+export const selectContacts = state => state.contacts.items;
+export const selectIsLoading = state => state.contacts.isLoading;
+export const selectError = state => state.contacts.error;
+
+export const selectFilteredContacts = createSelector(
+  [selectContacts, selectFilter],
+  (contacts, filter) =>
+    contacts.filter(({ name }) => {
+      console.log(name);
+      if (name !== 'undefined') {
+        return name.toLowerCase().includes(filter.toLowerCase());
+      }
+      return contacts;
+    })
+);
