ec18de8836c3e30c9a54bb40e09b126909264878
Deniss Simonaits <dsimonaits@gmail.com>
2023-01-17 00:12:19 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,28 +1,34 @@
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { useEffect } from 'react';
+import { fetchContacts } from 'redux/operations';
+import { selectError } from 'redux/selectors';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import Section from './Section';
 import ContactForm from './ContactForm';
 import FilterByName from './Filter';
 import ContactList from './ContactList';
-import { fetchContacts } from 'redux/operations';
 
 const App = () => {
   const dispatch = useDispatch();
+  const error = useSelector(selectError);
 
   useEffect(() => {
     dispatch(fetchContacts());
   }, [dispatch]);
+
+  error !== null && toast('Something went wrong!');
 
   return (
     <>
       <Section title="Phonebook" border="false">
         <ContactForm />
       </Section>
-
       <Section title="Contacts" border="true">
         <FilterByName />
         <ContactList />
       </Section>
+      <ToastContainer />
     </>
   );
 };
